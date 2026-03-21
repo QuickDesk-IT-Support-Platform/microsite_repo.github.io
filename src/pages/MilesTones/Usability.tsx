8f98400799436126a2a2b3fc0fb6409a54d4421e
AzoN2525 <diogo.l.oliveira@ua.pt>
2026-03-21 17:31:30 +0000

    Usability page added

---
+++ src/pages/MilesTones/Usability.tsx
@@ -0,0 +1,72 @@
+// JSX-only component — explicit React import not required
+import Header from "../Header";
+
+import container_architecture from "../../assets/updated_container_diagram.png";
+export default function Usability() {
+
+
+
+  return (
+    <div className="flex min-h-screen ">
+      <Header />
+
+    <div className="flex pt-18 w-full">
+  
+  {/* main content */}
+    <div id="M3 - Usability and Digital Accessibility" className="w-full md:w-8/8 p-4 md:p-20 bg-white md:h-[calc(100vh-4rem)] overflow-y-auto">
+        
+          
+        <h1 className="font-bold text-4xl text-center md:text-start">M3 - Usability and Digital Accessibility</h1>        
+        
+        <div className="mt-10 justify-center flex flex-col items-center space-y-6">
+            
+            <div className="w-full max-w-5xl aspect-video overflow-hidden rounded-2xl shadow-lg">
+                <iframe
+                  src="https://www.canva.com/design/DAHDjaVxZrQ/rcp1H1SCP-t08pufnkHUmQ/edit"
+                  allowFullScreen
+                  loading="lazy"
+                  className="w-full h-full border-0"
+                ></iframe>
+            </div>
+
+        </div>
+
+        <section id="Updated Architecture" className={`my-10 p-5 rounded-2xl  shadow-2xl }`}>
+            <div className="flex flex-row  gap-3 items-center pb-3" >
+               <span className={`w-2   h-10 rounded-2xl bg-orange-600/80`}></span>
+              <h2 className="text-2xl font-extrabold ">Updated Architecture</h2>
+            </div>
+            <div className="flex flex-col ">
+
+              <h1 className="text-xl font-bold">Container-level Diagram</h1>
+              <div className="flex item-center justify-center mb-10 mt-5">
+                  <img src={container_architecture} alt="Container-level Diagram" className="scale-100 md:scale-70" />  
+              </div>
+
+            </div>
+
+          </section>
+          
+      <section id="Demo" className="my-10 p-5 rounded-2xl shadow-2xl flex flex-col items-center justify-center">
+        <div className="flex flex-row gap-3 self-start pb-3">
+          <span className="w-2 h-10 rounded-2xl bg-orange-600/80"></span>
+          <h2 className="text-2xl font-extrabold">APP CURRENT STATE</h2>
+        </div>
+        <div className="w-full max-w-5xl aspect-video overflow-hidden rounded-2xl shadow-lg ">
+          <iframe
+            src="https://drive.google.com/file/d/1GhVGcEgLZTNNQVK5O8-EEqbuPyX4aLNV/preview"
+            className="w-full h-full"
+            allow="autoplay"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </section>
+
+
+      </div>
+    </div>
+    
+    </div>
+   
+  )
+}
