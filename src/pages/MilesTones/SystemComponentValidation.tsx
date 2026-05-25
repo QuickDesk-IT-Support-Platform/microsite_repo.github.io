5cb4371c5763c2096d99ddf0e620a923b651105e
AzoN2525 <diogoliveira1510@gmail.com>
2026-05-25 17:09:03 +0100

    canva pres

--- src/pages/MilesTones/SystemComponentValidation.tsx
+++ src/pages/MilesTones/SystemComponentValidation.tsx
@@ -119,6 +119,17 @@ export default function SystemComponentValidation() {
             PEI Course presentation - 19/05/2026
           </p>
 
+          <div className="mt-10 justify-center flex flex-col items-center space-y-6">
+            <div className="w-full max-w-5xl aspect-video overflow-hidden rounded-2xl shadow-lg">
+              <iframe
+                src="https://www.canva.com/design/DAHJr-yc-hQ/gdUDknQgIRTndsaMyOULlw/view?embed"
+                allowFullScreen
+                loading="lazy"
+                className="w-full h-full border-0"
+              ></iframe>
+            </div>
+          </div>
+
           <section className="my-10 p-5 rounded-2xl shadow-2xl">
             <SectionTitle>Project Context</SectionTitle>
             <p className="text-sm md:text-xl leading-relaxed text-justify">
